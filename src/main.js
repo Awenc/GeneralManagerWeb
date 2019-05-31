b4a93599c56c2ccc28e880ffd2d79728c9e287df
chenchuanwen <291966285@qq.com>
2019-05-31 09:34:26 +0800

    修改axios

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import echarts from 'echarts'
+import echarts from 'echarts';
+import axios from 'axios';
 
-Vue.prototype.$echarts = echarts
-
-Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts;
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
 Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
